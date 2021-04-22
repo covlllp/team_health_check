4c513ba7013fea2a5e983ca7833364a6fa593019
Colin VanLang <colinvanlang@gmail.com>
2021-04-22 11:31:52 -0700

    add info

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def sanitize_row(row):
     row["2"] = float(row["2"])
     row["3"] = float(row["3"])
     total = row["1"] + row["2"] + row["3"]
-    row["score"] = (row["1"] * 1 + row["2"] * 2 + row["3"] * 3) / total
+    row["score"] = round((row["1"] * 1 + row["2"] * 2 + row["3"] * 3) / total, 2)
     row["Month"] = datetime.strptime(row["Month"], "%m/%d/%y")
     return row
 
@@ -34,7 +34,10 @@ def graph_data(data):
         score.append(row["score"])
 
     df = pandas.DataFrame({"category": category, "month": month, "score": score})
-    fig = px.line(df, x="month", y="score", color="category")
+    fig = px.line(
+        df, x="month", y="score", color="category", text="score", hover_name="category", range_y=[1,3]
+    )
+    fig.show()
     fig.write_html("index.html")
 
 
