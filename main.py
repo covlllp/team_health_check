24c0649aacd7ceaf2c83b919d64e3331783ac13c
Colin VanLang <colinvanlang@gmail.com>
2021-07-26 17:36:10 -0400

    initial update for august

--- main.py
+++ main.py
@@ -18,9 +18,21 @@ def sanitize_row(row):
     row["1"] = float(row["1"])
     row["2"] = float(row["2"])
     row["3"] = float(row["3"])
-    total = row["1"] + row["2"] + row["3"]
-    row["score"] = round((row["1"] * 1 + row["2"] * 2 + row["3"] * 3) / total, 2)
+    row["4"] = float(row["4"])
+    row["5"] = float(row["5"])
+    total = row["1"] + row["2"] + row["3"] + row["4"] + row["5"]
+    row["score"] = round(
+        (row["1"] * 1 + row["2"] * 2 + row["3"] * 3 + row["4"] * 4 + row["5"] * 5)
+        / total,
+        2,
+    )
     row["Month"] = datetime.strptime(row["Month"], "%m/%d/%y")
+    if row["Month"] < datetime.strptime("8/1/21", "%m/%d/%y"):
+        row["score"] = round(
+            (row["2"] * 1.5 + row["3"] * 3 + row["4"] * 4.5) / total,
+            2,
+        )
+
     return row
 
 
@@ -41,7 +53,7 @@ def graph_data(data):
         color="category",
         text="score",
         hover_name="category",
-        range_y=[0.9, 3.1],
+        range_y=[0.9, 5.1],
     )
     fig.show()
     fig.write_html("index.html")
